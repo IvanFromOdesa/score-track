14822d72104235734aa71a470e5f07e9b2f4e680
Ivan <krylosov382@gmail.com>
2024-10-27 20:52:39 +0200

    Fixes for the case where the user with the email has already been singed up and connects via Google OAuth2.

--- src/main/java/com/teamk/scoretrack/module/core/entities/user/fan/service/FanEntityService.java
+++ src/main/java/com/teamk/scoretrack/module/core/entities/user/fan/service/FanEntityService.java
@@ -4,6 +4,8 @@ import com.teamk.scoretrack.module.core.entities.user.client.service.AbstractCli
 import com.teamk.scoretrack.module.core.entities.user.fan.ctx.FanProcessingContext;
 import com.teamk.scoretrack.module.core.entities.user.fan.dao.FanDao;
 import com.teamk.scoretrack.module.core.entities.user.fan.domain.Fan;
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,35 @@ public class FanEntityService extends AbstractClientUserEntityService<Fan, FanDa
                 () -> update(processClientUser(ctx, new Fan()),
                 () -> dao.findByAuthenticationBean(ctx.authenticationBean()))
         );
+    }
+
+    @Override
+    protected Fan merge(Fan e, Fan byId) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+
+        mapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+
+        mapper.typeMap(Fan.class, Fan.class).addMappings(m -> {
+            m.skip(Fan::setProfile);
+            m.skip(Fan::setViewershipPlan);
+        });
+
+        if (e.getProfile() != null) {
+            mapper.map(e.getProfile(), byId.getProfile());
+        }
+
+        if (e.getViewershipPlan() != null) {
+            mapper.map(e.getViewershipPlan(), byId.getViewershipPlan());
+        }
+
+        mapper.map(e, byId);
+
+        byId.setAuthenticationBean(entityManager.merge(byId.getAuthenticationBean()));
+        byId.setProfile(entityManager.merge(byId.getProfile()));
+        byId.setViewershipPlan(entityManager.merge(byId.getViewershipPlan()));
+
+        return byId;
     }
 
     @Override
